40c376d2c0b4d341bc3f25fafc578a24287b2f10
sloan-j <sloanj205@gmail.com>
2018-07-14 01:17:05 -0400

    base class

---
+++ silver-bot.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class Silverbot (object):
+
+	def __init__(self, f_name = 'Undefined', l_name = 'Undefined', u_name = 'Undefined', passw = 'Undefined', bday = "1", byear = "1900"):
+		self.f_name = f_name
+		self.l_name = l_name
+		self.u_name = u_name
+		self.passw = passw
+		self.bday = bday
+		self.byear = byear
+
+
+
